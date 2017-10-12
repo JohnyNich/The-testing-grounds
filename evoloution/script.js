28d48c280001c272a90bca3c2b00be22d24bf364
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-10-12 20:16:41 +0800

    Added table section and revamped welcome
    I added the table section into ‘evolution’, which contains multiple
    “tables” and you have to choose the right one. Also, I revamped the
    welcome page with CSS and a logo.

--- evoloution/script.js
+++ evoloution/script.js
@@ -2,18 +2,25 @@ function changeText(element, text) { // This function will change the text of th
 	document.getElementById(element).innerHTML = text;
 }
 
-var clicks = 0
+function playSound(id) {
+	document.getElementById(id).play();
+}
+
+var clicks = 0;
 function doNotClick() {
-	clicks += 1
+	clicks += 1;
 	switch (clicks) {}
-	if (clicks == 1){
+	if (clicks === 1){
 		window.alert("Do not click this button. For clicking it is pointless.");
 	} else if (clicks >= 100) {
-		window.alert("Why are you still clicking? Are you expecting to find anything at 200 clicks? Current clicks: " + clicks)
+		window.alert("Why are you still clicking? Are you expecting to find anything at 200 clicks? Current clicks: " + clicks);
 	} else if (clicks >= 2) {
 		window.alert("Clicking is pointless, and nothing happens when you get to 100 clicks. Current clicks: " + clicks);
 	}
 }
+
+// Pre-jQuery variables
+var clapping = false; // This is for the slow clap
 
 // This is where jQuery code begins
 $(document).ready(function(){
@@ -25,5 +32,40 @@ $(document).ready(function(){
 	});
 	$("#text-entry-submit").click(function(){ // WHen the entry button is pressed...
 		$("#text-entry-result").text($("#text-entry").val());
+	});
+	
+	// This section deals with the slow clap
+	$("#slow_clap").click(function(){
+		if (clapping === false) {
+			$("#slow_clap").click(function(){
+				$(this).attr("src", "../assets/slow_clap.gif");
+				clapping = true;
+			});
+		} else {
+			$("#slow_clap").click(function(){
+				$(this).attr("src", "../assets/slow_clap.jpg");
+				clapping = false;	
+				});
+			}	
+		});
+		
+	// This section deals with the tables
+	$("#table1").click(function(){ // Table 1
+		console.log ("Clicked");
+		$("#table-result").text("Wrong table, you idiot");
+	});
+	$("#table2").click(function(){ // Table 2
+		console.log("Clicked");
+		$("#table-result").text("That's a ruddy maths thing. We don't deal with maths things here. They're boring.");
+	});
+	$("#table3").click(function(){ // Table 3
+		console.log("Clicked");
+		$("#table-result").text("That's a pear, you insolute sandwitch.");
+	});
+	
+	// This section deals with the curtain
+	$("#table4").click(function(){
+		$(this).attr("src", "../assets/table-correct.png");
+		playSound("zelda");
 	});
 });
